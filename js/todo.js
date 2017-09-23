09009f066e3757c05932a325bfc1915b069c4a90
mgaspari <marcoanthonygaspari@gmail.com>
2017-09-22 21:30:42 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  submitForm()
 });
 
 // define functions here
+var submitForm = () => {
+  $('form').on('submit', (e) => {
+    e.preventDefault()
+    let a = $('#item').val()
+    $('#list ol').append(`<li>${a}</li>`)
+
+  })
+}
